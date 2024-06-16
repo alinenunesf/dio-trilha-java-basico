8a7bc0e6e86cf8bd8e542c281a7aeb96e3c62c7f
alinenunesf <alinenunes3105@hotmail.com>
2024-06-16 20:09:10 -0300

    Smart tv completa

--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -3,13 +3,16 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
+        System.out.println("Volume atual: " + smartTv.volume);        
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
-        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("Novo volume: " + smartTv.volume);
 
-
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Novo canal: " + smartTv.canal);
 
         System.out.println("TV ligada: " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
