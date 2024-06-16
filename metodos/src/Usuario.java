8a9ee8142a71afcf184583f8a654af693d5182b6
alinenunesf <alinenunes3105@hotmail.com>
2024-06-16 20:42:06 -0300

    SmartTv funções

--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -3,22 +3,31 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
+        System.out.println("--------------------");
+        System.out.println("  SMART TV FUNÇÕES  ");
+        System.out.println("--------------------");
+
+    //Volume
         System.out.println("Volume atual: " + smartTv.volume);        
-        smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
         System.out.println("Novo volume: " + smartTv.volume);
 
+        System.out.println("--------------------");
+
+    //Canal
         System.out.println("Canal atual: " + smartTv.canal);
         smartTv.mudarCanal(13);
+        System.out.println("Mudando o canal...");
         System.out.println("Novo canal: " + smartTv.canal);
 
+        System.out.println("--------------------");
+
+
+    //Tv ligada/desligada
         System.out.println("TV ligada: " + smartTv.ligada);
-        System.out.println("Canal atual: " + smartTv.canal);
-        System.out.println("Volume atual: " + smartTv.volume);
-       
         smartTv.ligar();
-        System.out.println("Novo status -> Tv ligada: " + smartTv.ligada);
+        System.out.println("Novo status da Tv: " + smartTv.ligada);
     }
 }
