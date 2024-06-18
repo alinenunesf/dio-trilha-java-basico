59c451c67ed6b02c1f6051be31f2d26e87711779
alinenunesf <alinenunes3105@hotmail.com>
2024-06-17 22:00:33 -0300

    if-else

--- Controle-fluxo/src/CaixaEletronico.java
+++ Controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 27.0;
+        double valorSolicitado = 30.0;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo " + saldo + " reais");
+        }
+        else 
+            System.out.println("Saldo insuficiente");
 
-        System.out.println(saldo);
+       
     }
 }
