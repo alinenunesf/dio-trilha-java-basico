a28e8e75c1361adfe2f1b583de1dd3261e99661e
alinenunesf <alinenunes3105@hotmail.com>
2024-06-18 11:05:46 -0300

    condicionais encadeadas

--- Controle-fluxo/src/ResultadoEscolar.java
+++ Controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,21 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 4;
+        System.out.println("Sua nota: " + nota);
 
-        if (nota >= 7)
+        if (nota >= 9)
+            System.out.println("Parabéns! Aprovado");
+
+        else if (nota >= 7 && nota < 9)
             System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+                System.out.println("Recuperação");    
 
         else
             System.out.println("Reprovado");
+
+            
 
     }
 }
