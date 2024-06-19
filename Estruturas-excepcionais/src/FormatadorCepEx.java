7fb81c8724653c35a65fd6e2e49190c5e717dd66
alinenunesf <alinenunes3105@hotmail.com>
2024-06-18 21:10:49 -0300

    exceções customizadas

---
+++ Estruturas-excepcionais/src/FormatadorCepEx.java
@@ -0,0 +1,18 @@
+public class FormatadorCepEx {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+            
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep nao corresponde com as regras");
+           
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if (cep.length()!=8)
+            throw new CepInvalidoException();
+
+            return "23.765-064";
+    }
+}
